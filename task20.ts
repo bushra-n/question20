05673b8d456eb10913325fd343bc5eae3767f62e
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-16 16:47:41 +0500

    Add files via upload

---
+++ task20.ts
@@ -0,0 +1,10 @@
+// task 20 Think of something you could store in an array e.g mountain,river,languages etc
+let languages:string[]= ["English","Urdu","Arabic","Sindhi","Chinese"];
+console.log("List of languages:");
+console.log(languages);
+
+
+
+
+
+
